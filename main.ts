32840b5d2a8cfc7ce7d4bce2670d052ca9152a2e
Amri Hidayat <amri.hidayat@gmail.com>
2021-08-27 16:30:02 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,86 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    music.pewPew.play()
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . 9 9 9 9 . . . . 
+        . . . . . . 9 9 9 9 5 9 . . . . 
+        . 9 9 9 9 9 9 9 5 5 9 9 . . . . 
+        . . . . . . . 9 9 9 9 . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, mySprite, 200, 0)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprite.destroy()
+    otherSprite.destroy()
+    info.changeScoreBy(1)
+    music.beamUp.play()
+    if (info.score() == 20) {
+        game.over(true)
+    }
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    music.zapped.play()
+    info.changeLifeBy(-1)
+    otherSprite.destroy(effects.disintegrate, 500)
+    scene.cameraShake(4, 500)
+})
+let enemyShip: Sprite = null
+let elapsedTime = 0
+let projectile: Sprite = null
+let mySprite: Sprite = null
+effects.starField.startScreenEffect()
+mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . 6 4 4 4 
+    . . . . . . . . . . 6 6 6 6 6 . 
+    . . . . . . . . 6 6 6 6 6 . . . 
+    . . . . . . . 6 6 6 6 6 6 . . . 
+    . . . . 6 6 6 6 6 6 6 6 . . . . 
+    . . 8 8 8 8 8 8 8 8 8 a . . . . 
+    6 6 6 6 6 6 6 6 6 6 a a a . . . 
+    . 6 6 6 6 6 6 6 6 6 a a a . . . 
+    . . 8 8 8 8 8 8 8 8 8 a . . . . 
+    . . . . . 6 6 6 6 6 6 6 6 6 . . 
+    . . . . . . . . . . 6 6 6 6 6 . 
+    . . . . . . . . . . . . . 4 4 4 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite, 100, 100)
+mySprite.setFlag(SpriteFlag.StayInScreen, true)
+info.setLife(10)
+game.onUpdateInterval(2000, function () {
+    elapsedTime = game.runtime()
+    enemyShip = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . 2 2 . . . . . . 
+        . . . . . . . . 3 2 . . . . . . 
+        . . . . . . . . 3 2 4 . . . . . 
+        . . . . . . . 3 3 2 . . . . . . 
+        . . . . . . . 3 2 2 . . . . . . 
+        . . . . . . 3 2 2 2 . . . . . . 
+        . . . . . 3 3 2 2 2 . . . . . . 
+        . . . 2 3 3 2 2 2 2 . . . . . . 
+        . . 2 2 2 2 2 2 2 2 . . . . . . 
+        . . . . . 3 2 2 2 2 . . . . . . 
+        . . . . . 3 3 2 2 2 . . . . . . 
+        . . . . . . . 3 3 2 4 . . . . . 
+        . . . . . . . . 3 2 . . . . . . 
+        . . . . . . . . . 2 . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    enemyShip.x = scene.screenWidth()
+    enemyShip.vx = -1 * randint(20, 60)
+    enemyShip.y = randint(10, scene.screenHeight() - 10)
+})
